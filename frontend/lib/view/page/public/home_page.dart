5240017f9cf297174e28f92758d87bb7d5e3407e
an920107 <an920107@gmail.com>
2024-02-27 02:57:24 +0800

    CHORE: flutter suggestion

--- frontend/lib/view/page/public/home_page.dart
+++ frontend/lib/view/page/public/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:health_care_website/model/post/post.dart';
 import 'package:health_care_website/view/widget/base/base_scaffold.dart';
