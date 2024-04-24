6420135037565bc5f3bfd6a70d57b3b84d91ed5d
an920107 <an920107@gmail.com>
2024-04-24 23:20:37 +0800

    add: prevent unauthorized user from accessing high permission areas.

--- frontend/lib/router/router.dart
+++ frontend/lib/router/router.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:health_care_website/enum/page_topic.dart';
+import 'package:health_care_website/enum/user_role.dart';
 import 'package:health_care_website/router/routes.dart';
-import 'package:health_care_website/view/page/private/admin_page.dart';
 import 'package:health_care_website/view/page/private/carousel_page.dart';
 import 'package:health_care_website/view/page/private/permission_page.dart';
 import 'package:health_care_website/view/page/private/static_page_edit_page.dart';
@@ -14,6 +15,8 @@ import 'package:health_care_website/view/page/public/post_page.dart';
 import 'package:health_care_website/view/page/public/redirect_page.dart';
 import 'package:health_care_website/view/page/public/restaurant_page.dart';
 import 'package:health_care_website/view/page/public/static_page_page.dart';
+import 'package:health_care_website/view_model/auth_view_model.dart';
+import 'package:provider/provider.dart';
 
 class Router {
   static GoRouter get router => GoRouter(
@@ -41,30 +44,31 @@ class Router {
 
           // 後台
           GoRoute(
-            path: Routes.admin.path,
-            builder: (context, state) => const AdminPage(),
-          ),
-          GoRoute(
             path: Routes.carousel.path,
             builder: (context, state) => const CarouselPage(),
+            redirect: _authCheckAndRedirect,
           ),
           GoRoute(
             path: Routes.postList.path,
             builder: (context, state) => const PostListPage(),
+            redirect: _authCheckAndRedirect,
           ),
           GoRoute(
             path: "${Routes.postEdit.path}/:id",
             builder: (context, state) =>
                 PostEditPage(state.pathParameters["id"]!),
+            redirect: _authCheckAndRedirect,
           ),
           GoRoute(
             path: Routes.restaurantList.path,
             builder: (context, state) => const RestaurantListPage(),
+            redirect: _authCheckAndRedirect,
           ),
           GoRoute(
             path: "${Routes.restaurantEdit.path}/:id",
             builder: (context, state) =>
                 RestaurantEditPage(state.pathParameters["id"]!),
+            redirect: _authCheckAndRedirect,
           ),
           GoRoute(
             path: Routes.pageEdit.path,
@@ -80,10 +84,12 @@ class Router {
               );
               return StaticPageEditPage(topic);
             },
+            redirect: _authCheckAndRedirect,
           ),
           GoRoute(
             path: Routes.permission.path,
             builder: (context, state) => const PermissionPage(),
+            redirect: _authCheckAndRedirect,
           ),
 
           // 登入相關
@@ -101,4 +107,22 @@ class Router {
           )
         ],
       );
+
+  static String? _authCheckAndRedirect(
+    BuildContext context,
+    GoRouterState state,
+  ) {
+    UserRole? roleRequired;
+    final fullPath = state.fullPath ?? "";
+    for (var route in List.from(Routes.values)
+      ..sort((a, b) => b.path.length.compareTo(a.path.length))) {
+      if (fullPath.startsWith(route.path)) {
+        roleRequired = route.roleRequired;
+        break;
+      }
+    }
+    if (roleRequired == null) return Routes.root.path;
+    final role = context.read<AuthViewModel>().role;
+    return role < roleRequired ? Routes.root.path : null;
+  }
 }
--- frontend/lib/router/routes.dart
+++ frontend/lib/router/routes.dart
@@ -1,21 +1,23 @@
+import 'package:health_care_website/enum/user_role.dart';
+
 enum Routes {
-  root("/"),
-  post("/post"),
-  restaurant("/restaurant"),
-  page("/page"),
-  login("/login"),
-  logout("/logout"),
-  redirect("/redirect"),
-  admin("/admin"),
-  carousel("/admin/carousel"),
-  pageEdit("/admin/page/edit"),
-  restaurantList("/admin/restaurant"),
-  restaurantEdit("/admin/restaurant/edit"),
-  postList("/admin/post"),
-  postEdit("/admin/post/edit"),
-  permission("/admin/permission");
+  root("/", UserRole.none),
+  post("/post", UserRole.none),
+  restaurant("/restaurant", UserRole.none),
+  page("/page", UserRole.none),
+  login("/login", UserRole.none),
+  logout("/logout", UserRole.none),
+  redirect("/redirect", UserRole.none),
+  carousel("/admin/carousel", UserRole.studentB),
+  pageEdit("/admin/page/edit", UserRole.studentB),
+  restaurantList("/admin/restaurant", UserRole.studentB),
+  restaurantEdit("/admin/restaurant/edit", UserRole.studentB),
+  postList("/admin/post", UserRole.studentB),
+  postEdit("/admin/post/edit", UserRole.studentB),
+  permission("/admin/permission", UserRole.admin);
 
   final String path;
+  final UserRole roleRequired;
 
-  const Routes(this.path);
+  const Routes(this.path, this.roleRequired);
 }
