5a9210c355955ae086ccf8f698f58d9e079ef19c
an920107 <an920107@gmail.com>
2024-06-12 01:32:29 +0800

    add: button onClick callback.

--- frontend-v2/src/components/button.tsx
+++ frontend-v2/src/components/button.tsx
@@ -1,11 +1,21 @@
+import { MouseEventHandler } from "react";
+
 type Props = {
-    children: Readonly<React.ReactNode>;
+  className?: string;
+  children: Readonly<React.ReactNode>;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({children}: Props) {
+export default function Button({ className, children, onClick }: Props) {
   return (
-    <div className="flex flex-row items-center px-3 py-1 rounded-full hover:bg-opacity-5 hover:bg-black transition-all">
+    <div className={className}>
+      <button
+        className="flex flex-row items-center px-3 py-1 rounded-full hover:bg-opacity-5 hover:bg-black transition-colors"
+        type="button"
+        onClick={onClick}
+      >
         {children}
+      </button>
     </div>
   )
 }
