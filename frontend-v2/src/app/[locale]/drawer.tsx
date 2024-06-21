78df1704a8ed23169547418367678452c18f626d
an920107 <an920107@gmail.com>
2024-06-22 04:03:55 +0800

    refactor: using document listener to detect and close drawer.

--- frontend-v2/src/app/[locale]/drawer.tsx
+++ frontend-v2/src/app/[locale]/drawer.tsx
@@ -1,17 +1,30 @@
+import { useEffect } from "react";
+
 type Props = {
     children: Readonly<React.ReactNode>;
     isOpen: boolean;
-    closeCallback?: () => void;
+    onClose?: () => void;
   }
 
-export default function Drawer({ children, isOpen, closeCallback }: Props) {
+export default function Drawer({ children, isOpen, onClose: closeCallback }: Props) {
+  useEffect(() => {
+    const clickHandler = (event: MouseEvent) => {
+      if (!document.getElementById("drawer")?.contains(event.target as Node))
+        closeCallback?.();
+    };
+
+    if (isOpen)
+      document.addEventListener("click", clickHandler);
+
+    return () => {
+      document.removeEventListener("click", clickHandler);
+    };
+  });
+
   return (
     <>
-      <button
-        className={`fixed inset-0 bg-black transform ${isOpen ? "translate-x-0" : "-translate-x-full"} ${isOpen ? "opacity-50" : "opacity-0"} duration-500 transition-opacity ease-in-out`}
-        onClick={closeCallback}
-      ></button>
-      <div className={`fixed top-0 left-0 h-full bg-white w-72 rounded-r-xl transform ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out`}>
+      <div className={`fixed inset-0 bg-black ${isOpen ? "opacity-50 pointer-events-auto" : "opacity-0 pointer-events-none"} duration-500 transition-opacity ease-in-out`}></div>
+      <div id="drawer" className={`fixed top-0 left-0 h-full bg-white w-72 rounded-r-xl transform ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out`}>
         {children}
       </div>
     </>
