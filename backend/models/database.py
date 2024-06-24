eb630d0a5f1046bae691c4a2eb6f9092fdf4c386
xunhaoz <leo20020529@gmail.com>
2024-06-24 11:04:13 +0800

    feat: database

---
+++ backend/models/database.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+class SchemaMixin:
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    create_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    update_time = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
