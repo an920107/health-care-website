ca5e1f7cfda4a48406c43048b1190ae0a0483d8c
xunhaoz <leo20020529@gmail.com>
2024-06-24 12:37:09 +0800

    feat: CustomResponse

---
+++ backend/models/attachment_model.py
@@ -0,0 +1,11 @@
+from models.database import SchemaMixin, db
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Attachment(SchemaMixin, db.Model):
+    __tablename__ = 'attachment'
+
+    filename = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f'<Attachment {self.filename}>'
--- backend/models/database.py
+++ backend/models/database.py
@@ -6,8 +6,8 @@ db = SQLAlchemy()
 
 class SchemaMixin:
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    create_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    update_time = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+    created_time = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated_time = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
     def to_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
