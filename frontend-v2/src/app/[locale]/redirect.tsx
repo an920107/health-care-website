ab8a3ac31039071b61b08e34c351dc6024ab76d8
an920107 <an920107@gmail.com>
2024-07-23 02:58:50 +0800

    fix: auto locale redirect

--- frontend-v2/src/app/[locale]/redirect.tsx
+++ frontend-v2/src/app/[locale]/redirect.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { locales } from '@/i18n';
 import { redirect } from '@/navigation';
 import { useSearchParams } from 'next/navigation';
 
-export default function NotFoundRedirect() {
+export default function HomeRedirect() {
     const query = useSearchParams();
     const notfoundPath = query.get("notfound");
-    if (notfoundPath !== null) redirect(`/404?notfound=${notfoundPath}`);
+    if (notfoundPath !== null) {
+        for (var locale of locales)
+            if (notfoundPath.startsWith(`/${locale}`))
+                redirect(`/404?notfound=${notfoundPath}`);
+        redirect(notfoundPath);
+    };
 
     return (
         <></>
