e1a585ba8071b5f02029f64e8ca12c0e5146893d
an920107 <an920107@gmail.com>
2024-07-23 02:07:17 +0800

    POST-EDITOR fix: attachment preview layout

--- frontend-v2/src/components/attachment-preview.tsx
+++ frontend-v2/src/components/attachment-preview.tsx
@@ -4,7 +4,6 @@ import AttachmentEntity from "@/module/attachment/domain/attachmentEntity";
 import UploadingAttachmentEntity from "@/module/attachment/domain/uploadingAttachmentEntity";
 import UploadingPregressMap from "@/module/attachment/domain/uploadingProgressMap";
 import { Table } from "@radix-ui/themes";
-import { useEffect } from "react";
 import Button from "./button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown, faArrowUp, faClose } from "@fortawesome/free-solid-svg-icons";
@@ -32,62 +31,63 @@ export default function AttachmentPreview({
   return (
     <div>
       {label && <label htmlFor={label} className="label">{label}</label>}
-      <div className={`${className ?? ""} flex flex-col items-center w-full rounded-lg border p-2 gap-2`}>
-        {
-          attachments.length === 0 && uploadingAttachments.length === 0
-            ? <p className="py-12">{trans("not_found")}</p>
-            : (
-              <Table.Root className="w-full">
-                <Table.Header>
-                  <Table.Row>
-                    <Table.ColumnHeaderCell>{trans("table_order")}</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell>{trans("table_filename")}</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell>{`${trans("table_time")} / ${trans("table_pregress")}`}</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell>{trans("table_move")}</Table.ColumnHeaderCell>
-                    <Table.ColumnHeaderCell>{trans("table_delete")}</Table.ColumnHeaderCell>
-                  </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                  {
-                    attachments.map((entity, index) => {
-                      const vm = new AttachmentViewModel(entity);
-                      return (
-                        <Table.Row>
-                          <Table.RowHeaderCell>{index + 1}</Table.RowHeaderCell>
-                          <Table.Cell>{vm.filename}</Table.Cell>
-                          <Table.Cell>{vm.uploadedTime}</Table.Cell>
-                          <Table.Cell>
-                            <div className="flex flex-row">
-                              <Button><FontAwesomeIcon icon={faArrowUp} className="size-4" /></Button>
-                              <Button><FontAwesomeIcon icon={faArrowDown} className="size-4" /></Button>
-                            </div>
-                          </Table.Cell>
-                          <Table.Cell>
-                            <Button><FontAwesomeIcon icon={faClose} className="size-4" /></Button>
-                          </Table.Cell>
-                        </Table.Row>
-                      );
-                    })
-                  }
-                  {
-                    uploadingAttachments.map((entity, index) => {
-                      const vm = new UploadingAttachmentViewModel(entity, uploadingProgressMap);
-                      return (
-                        <Table.Row>
-                          <Table.RowHeaderCell>-</Table.RowHeaderCell>
-                          <Table.Cell>{vm.filename}</Table.Cell>
-                          <Table.Cell>{`${vm.pregress.toFixed(2)} %`}</Table.Cell>
-                          <Table.Cell>-</Table.Cell>
-                          <Table.Cell>-</Table.Cell>
-                        </Table.Row>
-                      );
-                    })
-                  }
-                </Table.Body>
-              </Table.Root>
-            )
-        }
-      </div>
+      {
+        attachments.length === 0 && uploadingAttachments.length === 0
+          ?
+          <div className={`${className ?? ""} flex flex-col items-center w-full rounded-lg border p-2 gap-2`}>
+            <p className="py-12">{trans("not_found")}</p>
+          </div>
+          : (
+            <Table.Root className="w-full" variant="surface">
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColumnHeaderCell>{trans("table_order")}</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>{trans("table_filename")}</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>{`${trans("table_time")} / ${trans("table_progress")}`}</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>{trans("table_move")}</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>{trans("table_delete")}</Table.ColumnHeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {
+                  attachments.map((entity, index) => {
+                    const vm = new AttachmentViewModel(entity);
+                    return (
+                      <Table.Row align="center">
+                        <Table.RowHeaderCell>{index + 1}</Table.RowHeaderCell>
+                        <Table.Cell>{vm.filename}</Table.Cell>
+                        <Table.Cell>{vm.uploadedTime}</Table.Cell>
+                        <Table.Cell>
+                          <div className="flex flex-row">
+                            <Button><FontAwesomeIcon icon={faArrowUp} className="size-3 py-1.5" /></Button>
+                            <Button><FontAwesomeIcon icon={faArrowDown} className="size-3 py-1.5" /></Button>
+                          </div>
+                        </Table.Cell>
+                        <Table.Cell>
+                          <Button><FontAwesomeIcon icon={faClose} className="size-3 py-1.5" /></Button>
+                        </Table.Cell>
+                      </Table.Row>
+                    );
+                  })
+                }
+                {
+                  uploadingAttachments.map((entity, index) => {
+                    const vm = new UploadingAttachmentViewModel(entity, uploadingProgressMap);
+                    return (
+                      <Table.Row>
+                        <Table.RowHeaderCell>-</Table.RowHeaderCell>
+                        <Table.Cell>{vm.filename}</Table.Cell>
+                        <Table.Cell>{`${vm.pregress.toFixed(2)} %`}</Table.Cell>
+                        <Table.Cell>-</Table.Cell>
+                        <Table.Cell>-</Table.Cell>
+                      </Table.Row>
+                    );
+                  })
+                }
+              </Table.Body>
+            </Table.Root>
+          )
+      }
     </div>
   );
 }
