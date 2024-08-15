5a1157694d39161086e70de311d4fed0fd750fb4
Xunhaoz <leo20020529@gmail.com>
2024-08-16 01:18:08 +0800

    refactor python version

--- backend/models/insurance_model.py
+++ backend/models/insurance_model.py
@@ -5,21 +5,31 @@ from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.ext.mutable import MutableList
 
 
-class Post(SchemaMixin, db.Model):
-    __tablename__ = 'post'
+class Insurance(SchemaMixin, db.Model):
+    __tablename__ = 'insurance'
 
-    title = db.Column(db.String(255), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-
-    title_en = db.Column(db.String(255), nullable=False)
-    content_en = db.Column(db.Text, nullable=False)
-
-    column = db.Column(db.String(255), nullable=False)
-    attachments = db.Column(MutableList.as_mutable(JSON), nullable=False, default=[])
-    importance = db.Column(db.Boolean, default=False)
-    visibility = db.Column(db.Boolean, default=False)
+    application_date = db.Column(db.DateTime)
+    incident_date = db.Column(db.DateTime)
+    name = db.Column(db.String(40))
+    student_id = db.Column(db.String(40))
+    id_number = db.Column(db.String(40))
+    address = db.Column(db.String(60))
+    phone_number = db.Column(db.String(40))
+    email = db.Column(db.String(120))
+    claim_details = db.Column(db.String(40))
+    payment_type = db.Column(db.String(40))
+    location = db.Column(db.String(40))
+    incident_cause = db.Column(db.TEXT)
+    receipt = db.Column(db.TEXT)
+    diagnosis_certificate = db.Column(db.TEXT)
+    bankbook = db.Column(db.Integer)
+    x_ray = db.Column(db.Integer)
+    application_amount = db.Column(db.Integer)
+    claim_amount = db.Column(db.Integer)
+    claim_date = db.Column(db.DateTime)
+    remarks = db.Column(db.TEXT)
+    insurance_company_stamp = db.Column(db.Boolean)
 
     def __repr__(self):
-        return f'<Carousel {self.title}>'
-
+        return f'<Insurance {self.name}>'
 
