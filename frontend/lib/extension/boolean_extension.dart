d71a50810ced1b3e3db3dc903eee7a3e366cc604
an920107 <an920107@gmail.com>
2024-11-11 01:25:53 +0800

    remove deprecated code

--- frontend/lib/extension/boolean_extension.dart
+++
@@ -1,5 +0,0 @@
-extension BooleanExtension on bool {
-  int toZeroOne() {
-    return this ? 1 : 0;
-  }
-}
--- frontend/lib/view_model/mixin/title_validator.dart
+++
@@ -1,9 +0,0 @@
-mixin class TitleValidator {
-  String? titleValidator(String? text, [int maxLength = 25]) {
-    if (text == null) return "標題不可為空";
-    text = text.trim();
-    if (text.isEmpty) return "標題不可為空";
-    if (text.length > maxLength) return "標題不可超過 $maxLength 字";
-    return null;
-  }
-}
